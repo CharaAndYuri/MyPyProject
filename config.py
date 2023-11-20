6ac78b4f9ac286ed5a8348ef576d4cca2698d4bd
CharaAndYuri <150055594+CharaAndYuri@users.noreply.github.com>
2023-11-20 18:31:22 +0400

    Update config.py
    updated confoig

---
+++ config.py
@@ -0,0 +1,16 @@
+CELL_SIZE = 16
+
+CELL_ON_WIDTH = 32
+CELL_ON_HEIGHT = 32
+
+WIDTH = CELL_SIZE * CELL_ON_WIDTH
+HEIGHT = CELL_SIZE * CELL_ON_HEIGHT
+FRAMERATE = 60
+
+COLORS = {
+    "Black": (0, 0, 0),
+    "White": (255, 255, 255),
+    "Red": (255, 0, 0),
+    "Green": (0, 255, 0),
+    "Blue": (0, 0, 255)
+}
